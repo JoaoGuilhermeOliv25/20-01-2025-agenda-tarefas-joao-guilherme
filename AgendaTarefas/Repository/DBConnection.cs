f656600da9188db99d19c05068b18a1bc1e13daa
JoaoGuilhermeOliv25 <joaoguilhermeoliv25@gmail.com>
2026-01-20 20:30:07 -0300

    T001 - Criação da classe DBConnection e criação do método de conexão com o BD

---
+++ AgendaTarefas/Repository/DBConnection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace AgendaTarefas.Repository
+{
+    public class DBConnection
+    {
+        public static string connectionString = "Data Source=AgendaTarefas.db;Version=3;";
+
+
+        // Conexão com o BD
+        public static SQLiteConnection GetConnection()
+        {
+            return new SQLiteConnection(connectionString);
+        }
+    }
+}
