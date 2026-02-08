9750eb8511c1dcfb35d2a93c3e7b958fc49fc36a
JoaoGuilhermeOliv25 <joaoguilhermeoliv25@gmail.com>
2026-02-08 18:48:12 -0300

    T005 - Criação da classe AplicacaoConfigService

---
+++ AgendaTarefas/Services/AplicacaoConfigService.cs
@@ -0,0 +1,31 @@
+﻿using AgendaTarefas.Infrastructure.Configuration;
+using AgendaTarefas.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AgendaTarefas.Services
+{
+    public class AplicacaoConfigService
+    {
+        private AppSettings config = SettingsManager.CarregarConfig();
+
+        public void AplicarConfigs()
+        {
+
+            if (config.iniciarWindows)
+            {
+                
+            }
+            else
+            {
+
+
+
+            }
+        }
+    }
+}
