6cf77bb15eff3598d09269aad4410bbc4b026552
JoaoGuilhermeOliv25 <joaoguilhermeoliv25@gmail.com>
2026-02-08 12:10:03 -0300

    T005 - Criação da pasta System e da classe StartupManager para gerenciar a inicialização do programa

---
+++ AgendaTarefas/System/StartupManager.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaTarefas.System
+{
+    public static class StartupManager
+    {
+        private const string appNome = "Agenda Tarefas";
+
+        public static void HabilitarInicializacao()
+        {
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(
+                 @"Software\Microsoft\Windows\CurrentVersion\Run", true))
+            {
+                key.SetValue(appNome, Assembly.GetExecutingAssembly().Location);
+            }
+        } 
+    }
+}
